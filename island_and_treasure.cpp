b47de339d587fd6a7dc787dec3e4f3bf9764bf07
Thuần Trần <thuantran2704@gmail.com>
2025-03-29 21:23:05 -0400

    island and treasure

---
+++ island_and_treasure.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printGrid(const vector<vector<int>>& grid) {
+    for (const auto& row : grid) {
+        for (int cell : row) {
+            cout << cell << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    Solution solution;
+
+    vector<vector<int>> grid = {
+        {2147483647, -1, 0, 2147483647},
+        {2147483647, 2147483647, 2147483647, -1},
+        {2147483647, -1, 2147483647, -1},
+        {0, -1, 2147483647, 2147483647}
+    };
+
+    cout << "Original Grid:" << endl;
+    printGrid(grid);
+
+    solution.islandsAndTreasure(grid);
+
+    cout << "\nUpdated Grid:" << endl;
+    printGrid(grid);
+
+    return 0;
+}
