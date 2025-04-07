beb1248caaeb9cf003ad0f37e78b89b1248a773f
Thuần Trần <thuantran2704@gmail.com>
2025-04-07 00:18:12 -0400

    416 1D DP

---
+++ 416.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        bool canPartition(vector<int>& nums) {
+            int sum = accumulate(nums.begin(), nums.end(), 0);
+            if (sum % 2 != 0) return false;
+            
+            int target = sum / 2;
+            vector<bool> dp(target + 1, false);
+            dp[0] = true; // Base case: sum 0 is always achievable
+    
+            for (int num : nums) {
+                for (int j = target; j >= num; j--) {
+                    dp[j] = dp[j] || dp[j - num];
+                }
+            }
+            return dp[target];
+        }
+    };
+    
