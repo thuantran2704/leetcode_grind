509329c032c091f2971a4605dbe61d8378eef7fa
Thuan Tran <tqt@usf.edu>
2025-08-11 11:16:36 -0400

    1650, delete github actions, its been acting up

---
+++ 1650.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+    public:
+        Node* lowestCommonAncestor(Node* p, Node* q) {
+            Node* a = p;
+            Node* b = q;
+            while (a != b) {
+                a = (a == nullptr) ? q : a->parent;
+                b = (b == nullptr) ? p : b->parent;
+            }
+            return a; // or b, same thing
+        }
+    };
+    
