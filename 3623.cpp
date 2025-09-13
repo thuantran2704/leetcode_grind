413786291407a79755ef20b2e6920b0738e2dd76
Thuần Trần <thuantran2704@gmail.com>
2025-09-13 14:54:58 -0400

    fix 3623 shorter

--- 3623.cpp
+++ 3623.cpp
@@ -1,35 +1,16 @@
-#define MOD 1000000007
 class Solution {
 public:
-    int choose2(int n){
-        if (n < 2) return 0;
-        long long temp = (long long) n * (n - 1) / 2;
-        temp %= MOD;
-        return (int) temp;
-    }
     int countTrapezoids(vector<vector<int>>& points) {
-        unordered_map<int,int> lines;
-        for(auto& point: points){
-            ++lines[point[1]];
+        int MOD = 1000000007;
+        unordered_map<int, int> groups;
+        for (auto& point : points)
+            groups[point[1]]++;
+        long long res = 0, total = 0;
+        for (auto& group : groups){
+            long long lines = 1LL* group.second * (group.second - 1) / 2;
+            res = (res + total * lines) % MOD;
+            total = (total + lines) % MOD;
         }
-        
-        unordered_map<int,int> linePairs;
-        int sumPairs = 0;
-        for(auto& p: lines){
-            int c2 = choose2(p.second);
-            linePairs[p.first] = c2;
-            sumPairs = (sumPairs + c2) % MOD;
-        }
-        
-        int ans = 0;
-        for(auto& p: linePairs){
-            int otherPairs = sumPairs - p.second;
-            if(otherPairs < 0) otherPairs += MOD;
-            ans = (ans + (long long)p.second * otherPairs) % MOD;
-        }
-        const int inv2 = 500000004;
-        ans = (int)((long long)ans * inv2 % MOD);
-        
-        return ans;
+        return (int)res;
     }
 };
